c2de198ddd0b2f5ae33220c2090f85f5c1c74382
Christian Gil <christian@chrisgvdev.com>
2023-04-17 19:47:09 -0600

    update: Improve the marquee test page

--- playground/pages/marquee.vue
+++ playground/pages/marquee.vue
@@ -3,9 +3,17 @@ const marqueeWords = ['MARQUEE', 'COOL', 'YEAH'];
 </script>
 
 <template>
-  <UiMarquee :speed="200" reverse>
-    <h4 class="text-primary-500 mr-8 text-[clamp(9rem,15vw,50vh)] leading-none">
-      <template v-for="w in marqueeWords">{{ w }}{{ ' - ' }}</template>
-    </h4>
-  </UiMarquee>
+  <main>
+    <UiMarquee :speed="120">
+      <h4 class="text-primary-500 mr-8 text-[clamp(9rem,15vw,50vh)] leading-none">
+        <template v-for="w in marqueeWords">{{ w }}{{ ' - ' }}</template>
+      </h4>
+    </UiMarquee>
+
+    <UiMarquee :speed="80" reverse>
+      <h4 class="text-primary-900 mr-8 text-[clamp(6rem,10vw,15vh)] leading-none">
+        <template v-for="w in marqueeWords">{{ w }}{{ ' - ' }}</template>
+      </h4>
+    </UiMarquee>
+  </main>
 </template>
