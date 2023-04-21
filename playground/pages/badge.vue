2dc86d3398d087d26d7923d08453b8657901d22b
Christian Gil <christian@chrisgvdev.com>
2023-04-21 12:59:51 -0600

    update: Add variants to the badges

--- playground/pages/badge.vue
+++ playground/pages/badge.vue
@@ -9,45 +9,165 @@ function dismiss() {
     <div>
       <h1 class="text-2xl">Badges</h1>
 
-      <div class="mt-4 flex items-center gap-4">
-        <span class="text-sm text-gray-500">Normal</span>
-        <UiBadge class="bg-primary-100 text-primary-800">Badge</UiBadge>
-        <UiBadge size="lg" class="bg-primary-100 text-primary-800">Badge</UiBadge>
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">Normal</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge color="gray">Badge</UiBadge>
+          <UiBadge color="red">Badge</UiBadge>
+          <UiBadge color="yellow">Badge</UiBadge>
+          <UiBadge color="green">Badge</UiBadge>
+          <UiBadge color="blue">Badge</UiBadge>
+          <UiBadge color="indigo">Badge</UiBadge>
+          <UiBadge color="purple">Badge</UiBadge>
+          <UiBadge color="pink">Badge</UiBadge>
+        </div>
       </div>
 
-      <div class="mt-4 flex items-center gap-4">
-        <span class="text-sm text-gray-500">With dot</span>
-        <UiBadge dot class="bg-primary-100 text-primary-800">Badge</UiBadge>
-        <UiBadge dot size="lg" class="bg-primary-100 text-primary-800">Badge</UiBadge>
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">Small</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge small color="gray">Badge</UiBadge>
+          <UiBadge small color="red">Badge</UiBadge>
+          <UiBadge small color="yellow">Badge</UiBadge>
+          <UiBadge small color="green">Badge</UiBadge>
+          <UiBadge small color="blue">Badge</UiBadge>
+          <UiBadge small color="indigo">Badge</UiBadge>
+          <UiBadge small color="purple">Badge</UiBadge>
+          <UiBadge small color="pink">Badge</UiBadge>
+        </div>
       </div>
 
-      <div class="mt-4 flex items-center gap-4">
-        <span class="text-sm text-gray-500">Rounded</span>
-        <UiBadge rounded class="bg-primary-100 text-primary-800">Badge</UiBadge>
-        <UiBadge rounded size="lg" class="bg-primary-100 text-primary-800">Badge</UiBadge>
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">With dot</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge dot color="gray">Badge</UiBadge>
+          <UiBadge dot color="red">Badge</UiBadge>
+          <UiBadge dot color="yellow">Badge</UiBadge>
+          <UiBadge dot color="green">Badge</UiBadge>
+          <UiBadge dot color="blue">Badge</UiBadge>
+          <UiBadge dot color="indigo">Badge</UiBadge>
+          <UiBadge dot color="purple">Badge</UiBadge>
+          <UiBadge dot color="pink">Badge</UiBadge>
+        </div>
       </div>
 
-      <div class="mt-4 flex items-center gap-4">
-        <span class="text-sm text-gray-500">With action</span>
-        <UiBadge class="bg-primary-100 text-primary-800">
-          Badge
-          <template #action>
-            <button @click="dismiss">
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">No background</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge no-bg dot color="gray">Badge</UiBadge>
+          <UiBadge no-bg dot color="red">Badge</UiBadge>
+          <UiBadge no-bg dot color="yellow">Badge</UiBadge>
+          <UiBadge no-bg dot color="green">Badge</UiBadge>
+          <UiBadge no-bg dot color="blue">Badge</UiBadge>
+          <UiBadge no-bg dot color="indigo">Badge</UiBadge>
+          <UiBadge no-bg dot color="purple">Badge</UiBadge>
+          <UiBadge no-bg dot color="pink">Badge</UiBadge>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">Pill</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge pill color="gray">Badge</UiBadge>
+          <UiBadge pill color="red">Badge</UiBadge>
+          <UiBadge pill color="yellow">Badge</UiBadge>
+          <UiBadge pill color="green">Badge</UiBadge>
+          <UiBadge pill color="blue">Badge</UiBadge>
+          <UiBadge pill color="indigo">Badge</UiBadge>
+          <UiBadge pill color="purple">Badge</UiBadge>
+          <UiBadge pill color="pink">Badge</UiBadge>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">Flat</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge flat color="gray">Badge</UiBadge>
+          <UiBadge flat color="red">Badge</UiBadge>
+          <UiBadge flat color="yellow">Badge</UiBadge>
+          <UiBadge flat color="green">Badge</UiBadge>
+          <UiBadge flat color="blue">Badge</UiBadge>
+          <UiBadge flat color="indigo">Badge</UiBadge>
+          <UiBadge flat color="purple">Badge</UiBadge>
+          <UiBadge flat color="pink">Badge</UiBadge>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <span class="text-sm font-medium text-gray-600">Action</span>
+
+        <div class="mt-2 flex items-center gap-4">
+          <UiBadge small @click:action="dismiss" color="gray">
+            Badge
+
+            <template #action>
               <span class="sr-only">Close</span>
-              <Icon name="heroicons:x-mark-20-solid" class="h-4 w-4" />
-            </button>
-          </template>
-        </UiBadge>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="red">
+            Badge
 
-        <UiBadge rounded size="lg" class="bg-primary-100 text-primary-800">
-          Badge
-          <template #action>
-            <button @click="dismiss">
+            <template #action>
               <span class="sr-only">Close</span>
-              <Icon name="heroicons:x-mark-20-solid" class="h-4 w-4" />
-            </button>
-          </template>
-        </UiBadge>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="yellow">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="green">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="blue">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="indigo">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="purple">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+          <UiBadge @click:action="dismiss" color="pink">
+            Badge
+
+            <template #action>
+              <span class="sr-only">Close</span>
+              <Icon name="heroicons:x-mark" />
+            </template>
+          </UiBadge>
+        </div>
       </div>
     </div>
   </UiContainer>
--- src/runtime/components/Badge.vue
+++ src/runtime/components/Badge.vue
@@ -1,42 +1,116 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { computed, type PropType } from 'vue';
 
-defineProps({
+type BadgeColors = 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink';
+
+interface BadgeElements {
+  addons: string;
+  flat: string;
+  border: string;
+  action: string;
+}
+
+const props = defineProps({
   dot: Boolean,
-  rounded: Boolean,
-  size: { type: String as PropType<'sm' | 'lg'>, default: 'sm' },
+  pill: Boolean,
+  flat: Boolean,
+  small: Boolean,
+  noBg: Boolean,
+  color: { type: String as PropType<BadgeColors>, default: 'gray' },
+});
+defineEmits(['click:action']);
+
+const DEFAULT_COLOR_VARIANTS: Record<BadgeColors, BadgeElements> = {
+  blue: {
+    addons: 'text-blue-500',
+    action: 'hover:bg-blue-600/20',
+    flat: 'bg-blue-100 text-blue-700',
+    border: 'bg-blue-50 text-blue-700 ring-blue-700/10',
+  },
+  gray: {
+    addons: 'text-gray-400',
+    action: 'hover:bg-blue-500/20',
+    flat: 'bg-gray-100 text-gray-600',
+    border: 'bg-gray-50 text-gray-600 ring-gray-500/10',
+  },
+  green: {
+    addons: 'text-emerald-500',
+    action: 'hover:bg-green-600/20',
+    flat: 'bg-emerald-100 text-emerald-700',
+    border: 'bg-emerald-50 text-emerald-700 ring-emerald-600/20',
+  },
+  indigo: {
+    addons: 'text-indigo-500',
+    action: 'hover:bg-indigo-600/20',
+    flat: 'bg-indigo-100 text-indigo-700',
+    border: 'bg-indigo-50 text-indigo-700 ring-indigo-700/10',
+  },
+  pink: {
+    addons: 'text-pink-500',
+    action: 'hover:bg-pink-600/20',
+    flat: 'bg-pink-100 text-pink-700',
+    border: 'bg-pink-50 text-pink-700 ring-pink-700/10',
+  },
+  purple: {
+    addons: 'text-purple-500',
+    action: 'hover:bg-purple-600/20',
+    flat: 'bg-purple-100 text-purple-700',
+    border: 'bg-purple-50 text-purple-700 ring-purple-700/10',
+  },
+  red: {
+    addons: 'text-red-500',
+    action: 'hover:bg-red-600/20',
+    flat: 'bg-red-100 text-red-700',
+    border: 'bg-red-50 text-red-700 ring-red-600/10',
+  },
+  yellow: {
+    addons: 'text-yellow-500',
+    action: 'hover:bg-yellow-600/20',
+    flat: 'bg-yellow-100 text-yellow-800',
+    border: 'bg-yellow-50 text-yellow-800 ring-yellow-600/20',
+  },
+};
+
+const badgeColor = computed(() => {
+  if (props.noBg) return 'ring-1 ring-inset ring-gray-200';
+  else if (props.flat) return DEFAULT_COLOR_VARIANTS[props.color].flat;
+  else return `${DEFAULT_COLOR_VARIANTS[props.color].border} ring-1 ring-inset`;
 });
 </script>
 
 <template>
   <span
     :class="[
-      'inline-flex items-center py-0.5 font-medium',
-      size === 'sm' ? 'px-2.5 text-xs' : 'px-3 text-sm',
-      rounded ? 'rounded' : 'rounded-full',
+      'inline-flex items-center gap-x-1.5 text-xs font-medium',
+      pill ? 'rounded-full' : 'rounded-md',
+      small ? 'px-1.5 py-0.5' : 'px-2 py-1',
+      badgeColor,
     ]"
   >
-    <svg v-if="dot" viewBox="0 0 8 8" fill="currentColor" class="mr-1.5 h-2 w-2 text-current">
-      <circle cx="4" cy="4" r="3" />
+    <svg
+      v-if="dot"
+      viewBox="0 0 6 6"
+      aria-hidden="true"
+      fill="currentColor"
+      :class="['h-1.5 w-1.5', DEFAULT_COLOR_VARIANTS[color].addons]"
+    >
+      <circle cx="3" cy="3" r="3" />
     </svg>
 
     <slot />
 
-    <div v-if="$slots.action" class="cgvweb-badge__action">
+    <button
+      type="button"
+      v-if="$slots.action"
+      @click="$emit('click:action')"
+      :class="[
+        DEFAULT_COLOR_VARIANTS[color].addons,
+        DEFAULT_COLOR_VARIANTS[color].action,
+        small ? '-mr-0.5' : '-mr-1',
+        'flex h-3.5 w-3.5 shrink-0 items-center justify-center rounded-sm hover:bg-gray-900/5',
+      ]"
+    >
       <slot name="action" />
-    </div>
+    </button>
   </span>
 </template>
-
-<style>
-.cgvweb-badge__action > * {
-  margin-left: 0.125rem;
-  margin-right: -0.125rem;
-  display: flex;
-  height: 1rem;
-  width: 1rem;
-  flex-shrink: 0;
-  align-items: center;
-  justify-content: center;
-}
-</style>
