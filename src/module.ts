2790c0862c7c84785842110881388c7bbeca6b69
Christian Gil <christian@chrisgvdev.com>
2023-04-23 12:45:22 -0600

    update: Organize the components

--- src/module.ts
+++ src/module.ts
@@ -11,35 +11,35 @@ export default defineNuxtModule({
 
     addComponent({
       name: 'UiTooltip',
-      filePath: resolver.resolve('runtime/components/Tooltip'),
+      filePath: resolver.resolve('runtime/components/basic/Tooltip'),
     });
     addComponent({
       name: 'UiContainer',
-      filePath: resolver.resolve('runtime/components/Container.vue'),
+      filePath: resolver.resolve('runtime/components/basic/Container.vue'),
     });
     addComponent({
       name: 'UiMarquee',
-      filePath: resolver.resolve('runtime/components/Marquee.vue'),
+      filePath: resolver.resolve('runtime/components/basic/Marquee.vue'),
     });
     addComponent({
       name: 'UiHighlightText',
-      filePath: resolver.resolve('runtime/components/HighlightText'),
+      filePath: resolver.resolve('runtime/components/functional/HighlightText'),
     });
     addComponent({
       name: 'UiBreakpointViewer',
-      filePath: resolver.resolve('runtime/components/BreakpointViewer.vue'),
+      filePath: resolver.resolve('runtime/components/other/BreakpointViewer.vue'),
     });
     addComponent({
       name: 'UiBadge',
-      filePath: resolver.resolve('runtime/components/Badge.vue'),
+      filePath: resolver.resolve('runtime/components/basic/Badge.vue'),
     });
     addComponent({
       name: 'UiDialog',
-      filePath: resolver.resolve('runtime/components/Dialog.vue'),
+      filePath: resolver.resolve('runtime/components/basic/Dialog.vue'),
     });
     addComponent({
       name: 'UiSlideover',
-      filePath: resolver.resolve('runtime/components/Slideover.vue'),
+      filePath: resolver.resolve('runtime/components/basic/Slideover.vue'),
     });
 
     nuxt.hook('tailwindcss:config', function (tailwindConfig) {
